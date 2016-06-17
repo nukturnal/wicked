701f075f6d0ac79f7a0ea27d0d617b5f52a7bcf0
Alfred Rowe <alfred@encodevlabs.com>
2016-06-17 14:10:55 +0000

    Added last_step? helper

--- lib/wicked/wizard.rb
+++ lib/wicked/wizard.rb
@@ -26,7 +26,7 @@ module Wicked
       helper_method :wizard_path,     :next_wizard_path, :previous_wizard_path,
                     :step,            :wizard_steps,     :current_step?,
                     :past_step?,      :future_step?,     :previous_step?,
-                    :next_step?
+                    :next_step?,      :last_step?
       # Set @step and @next_step variables
       before_action :setup_wizard
     end
